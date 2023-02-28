1d36159122595b8e5e33e2b965af6666730c9b6d
Shashank Mishra <shashankmishra3082000@gmail.com>
2023-02-28 20:30:46 +0530

    Loops

---
+++ table.java
@@ -0,0 +1,22 @@
+package LOOPS;
+
+import java.util.Scanner;
+
+public class table {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            System.out.println("The table of "+ i +"is");
+            for(int k=1;k<=10;k++)
+            {
+                int l=i*k;
+                System.out.println(i+" * "+ k +" = "+l);
+            }
+            System.out.println();
+        }
+        }
+    
+    }
+}
